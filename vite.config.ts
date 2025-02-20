86581eb609b38189221717fbd802c0df79f2e274
NurKholis <kholis478@gmail.com>
2025-02-20 20:19:14 +0700

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    proxy: {
+      '/api': { // Ganti dengan prefix API yang digunakan
+        target: 'https://hon-jana-flickacademy-4e912e1a.koyeb.app', // Ganti dengan URL backend
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
